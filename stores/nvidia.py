3e938cf1a68faa7e64b5cd0e061e19e1dc0efa37
TerryFrench <tperraut@gmail.com>
2020-09-18 17:18:16 -0700

    changed user agent
    using Edge chromium. less common

--- stores/nvidia.py
+++ stores/nvidia.py
@@ -35,7 +35,8 @@ GPU_DISPLAY_NAMES = {
 
 DEFAULT_HEADERS = {
     "Accept": "application/json",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51",
+#"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
 }
 
 
